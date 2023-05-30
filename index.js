568cd1849767e355d65e6fb5c4fc6dc9240b5286
Muhammad Rafly Alfayed <mralfayed@gmail.com>
2023-05-30 20:27:30 +0800

    intial commit

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ require('dotenv').config(); // Load environment variables
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const routes = require('./routes');
+const userRoutes = require('./routes/userRoute');
+
+
 
 const app = express();
-const port = process.env.PORT || 3000; // Use the PORT variable from .env or default to 3000
+const port = process.env.APP_PORT || 3000; // Use the PORT variable from .env or default to 3000
 
 app.use(bodyParser.json());
-app.use('/', routes);
+app.use('/users', userRoutes);
+
+
+app.use('/', (req, res) => {
+    res.send({
+        message: "app lancar"
+    })
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
